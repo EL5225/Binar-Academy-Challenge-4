0e4148314d3713c5eac72d676f8027e66b2b1fac
EL5225 <luthfiramadhan155@gmail.com>
2023-10-08 22:57:14 +0700

    imporvement: middleware

--- controllers/profiles/index.js
+++ controllers/profiles/index.js
@@ -24,6 +24,7 @@ export const updateProfiles = async (req, res, next) => {
       data: updated,
     });
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,19 +1,53 @@
-export const errorHandler = (err, req, res, next) => {
-  switch (err.code) {
-    case "P2025":
-      return res.status(400).json({
-        status: false,
-        message: "Bad Request",
-        error: `Operation Failed because of ${err.meta.cause}`,
-      });
+import { Prisma } from "@prisma/client";
 
-    default:
-      return res.status(500).json({
-        status: false,
-        message: "Internal Server error",
-        error: err.message,
-      });
+export const errorHandler = (err, req, res, next) => {
+  return res.status(500).json({
+    status: false,
+    message: "Internal Server error",
+    error: err.message,
+  });
+};
+
+export const prismaErrorHandler = (err, req, res, next) => {
+  const error = err.message.split("\n");
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    return res.status(400).json({
+      status: false,
+      message: "Bad Request",
+      error: error[error.length - 1],
+    });
   }
+
+  if (err instanceof Prisma.PrismaClientRustPanicError) {
+    return res.status(400).json({
+      status: false,
+      message: "Bad Request",
+      error: error[error.length - 1],
+    });
+  }
+
+  if (err instanceof Prisma.PrismaClientUnknownRequestError) {
+    return res.status(400).json({
+      status: false,
+      message: "Bad Request",
+      error: error[error.length - 1],
+    });
+  }
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    return res.status(400).json({
+      status: false,
+      message: "Bad Request",
+      error: error[error.length - 1],
+    });
+  }
+
+  return res.status(503).json({
+    status: false,
+    message: "Something Bad Happened",
+    error: err.message,
+  });
 };
 
 export const notFound = (req, res, next) => {
